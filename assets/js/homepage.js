5ff2a63c4bc67996c220168c82e3c6da747bb8c2
htam <hdfair@gmail.com>
2022-01-27 11:33:38 -0500

    match chapter code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,22 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+
+        // Clear old content      
+        repoContainerEl.textContent = '';
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
 var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -12,6 +28,7 @@ var getUserRepos = function (user) {
         .then(function (response) {
             // request was successful
             if (response.ok) {
+                console.log(response);
                 response.json().then(function (data) {
                     displayRepos(data, user);
                 });
@@ -23,20 +40,6 @@ var getUserRepos = function (user) {
             // Notice this `.catch()` getting chained onto the end of the `.then()` method
             alert("Unable to connect to GitHub");
         });
-}
-
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    // get value from input element
-    var username = nameInputEl.value.trim();
-
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
-        alert("Please enter a GitHub username");
-    }
-    console.log(event);
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -45,11 +48,12 @@ var displayRepos = function (repos, searchTerm) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     }
-    console.log(repos);
-    console.log(searchTerm);
+
     // clear old content
     repoContainerEl.textContent = "";
+
     repoSearchTerm.textContent = searchTerm;
+
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
         // format repo name
@@ -86,4 +90,5 @@ var displayRepos = function (repos, searchTerm) {
     }
 };
 
+// Add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
